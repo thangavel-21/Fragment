5a5d2a806ed2806c8a3b6fcd10291e41114d5b89
thangavel-21 <thangavel@calibraint.com>
2022-08-23 13:20:54 +0530

    code refactor

--- app/src/main/java/com/terareum/fragments/DashboardActivity.kt
+++ app/src/main/java/com/terareum/fragments/DashboardActivity.kt
@@ -2,29 +2,30 @@ package com.terareum.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
 import androidx.fragment.app.Fragment
+import com.terareum.fragments.Fragments.NotesFragment
+import com.terareum.fragments.Fragments.RecorderFragment
 import com.terareum.fragments.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class DashboardActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         bottomTabNavigation()
-        supportFragmentManager.beginTransaction().replace(R.id.frame_layout, FragmentOne()).commit()
+        supportFragmentManager.beginTransaction().replace(R.id.frame_layout, NotesFragment()).commit()
     }
 
     private fun bottomTabNavigation() {
         binding.bottomNav.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.action_home -> {
-                    replace(FragmentOne())
+                    replace(NotesFragment())
                     true
                 }
                 R.id.action_more -> {
-                    replace(FragmentTwo())
+                    replace(RecorderFragment())
                     true
                 }
                 else -> {
--- app/src/main/java/com/terareum/fragments/Fragments/RecorderFragment.kt
+++ app/src/main/java/com/terareum/fragments/Fragments/RecorderFragment.kt
@@ -1,21 +1,21 @@
-package com.terareum.fragments
+package com.terareum.fragments.Fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.terareum.fragments.databinding.FragmentOneBinding
-import com.terareum.fragments.databinding.FragmentTwoBinding
+import com.terareum.fragments.R
+import com.terareum.fragments.databinding.RecorderFragmentBinding
 
-class FragmentTwo : Fragment() {
-    lateinit var binding: FragmentTwoBinding
+class RecorderFragment : Fragment() {
+    private lateinit var binding: RecorderFragmentBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentTwoBinding.inflate(inflater, container, false)
+        binding = RecorderFragmentBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -24,7 +24,7 @@ class FragmentTwo : Fragment() {
         binding.click.setOnClickListener {
          val bundle = Bundle()
             bundle.putString("key1","check")
-            val frag = FragmentOne()
+            val frag = NotesFragment()
             frag.arguments = bundle
             parentFragmentManager.beginTransaction().replace(R.id.frame_layout, frag).commit()
         }
